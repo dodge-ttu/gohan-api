9a643fb0b41c605ebe0bda5f9c058b05edefb89f
dodge-ttu <will@iamdodge.us>
2020-08-19 14:48:35 -0500

    Added create tag feature

--- app/devices/tests/test_devices_api.py
+++ app/devices/tests/test_devices_api.py
@@ -60,3 +60,23 @@ class PrivateTagApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        """Test creating a new tag"""
+        payload = {
+            'name': 'Soil Moisture Probe',
+        }
+        self.client.post(DEVICETAGS_URL, payload)
+        exists = Devicetag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_invalid_tag(self):
+        """Test creating an invalid tag fails"""
+        payload = {
+            'name': '',
+        }
+        res = self.client.post(DEVICETAGS_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
